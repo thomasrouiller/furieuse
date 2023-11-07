643eb723a8f52943747a62aa219579a7d697876c
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-11-07 16:14:44 +0100

    kindaworks

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,23 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#layer-1", {
-  y: "50px",
+gsap.from("#js-layer-2", {
+  y: 100,
   ease: "none",
   scrollTrigger: {
-    trigger: "#parallax",
+    trigger: "#js-parallax",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.from("#js-layer-7", {
+  y: 400,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#js-parallax",
     start: "top top",
     end: "bottom center",
     scrub: true,
@@ -14,11 +26,11 @@ gsap.from("#layer-1", {
   },
 });
 
-gsap.from("#layer-2", {
-  y: "250px",
+gsap.from("#js-layer-3", {
+  y: 100,
   ease: "none",
   scrollTrigger: {
-    trigger: "#parallax",
+    trigger: "#js-parallax",
     start: "top top",
     end: "bottom center",
     scrub: true,
@@ -26,27 +38,15 @@ gsap.from("#layer-2", {
   },
 });
 
-gsap.from("#layer-3", {
-  y: "500px",
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#parallax",
-    start: "top top",
-    end: "bottom center",
-    scrub: true,
-    markers: true,
-  },
-});
-
-gsap.to(".slider-track", {
-  scrollTrigger: {
-    trigger: ".slider-mask",
-    start: "top top",
-    end: "+=300%",
-    pin: true,
-    scrub: true,
-    markers: true,
-  },
-  x: "-75%",
-  ease: "sine.inOut",
-});
+// gsap.to(".slider-track", {
+//   scrollTrigger: {
+//     trigger: ".slider-mask",
+//     start: "top top",
+//     end: "+=300%",
+//     pin: true,
+//     scrub: true,
+//     markers: true,
+//   },
+//   x: "-75%",
+//   ease: "sine.inOut",
+// });
