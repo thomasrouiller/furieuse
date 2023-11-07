c62bb04282b0e4254a3d2d745625a5c4e1a2a3e2
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-11-07 17:00:49 +0100

    halloween

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,31 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from("#js-layer-1", {
+  y: 0,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#js-parallax",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
 gsap.from("#js-layer-2", {
+  y: 50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#js-parallax",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.from("#js-layer-3", {
   y: 100,
   ease: "none",
   scrollTrigger: {
@@ -14,8 +38,20 @@ gsap.from("#js-layer-2", {
   },
 });
 
-gsap.from("#js-layer-7", {
-  y: 400,
+gsap.from("#js-layer-4", {
+  y: 200,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#js-parallax",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.from("#js-layer-6", {
+  y: 300,
   ease: "none",
   scrollTrigger: {
     trigger: "#js-parallax",
@@ -26,8 +62,8 @@ gsap.from("#js-layer-7", {
   },
 });
 
-gsap.from("#js-layer-3", {
-  y: 100,
+gsap.from("#js-layer-7", {
+  y: 400,
   ease: "none",
   scrollTrigger: {
     trigger: "#js-parallax",
